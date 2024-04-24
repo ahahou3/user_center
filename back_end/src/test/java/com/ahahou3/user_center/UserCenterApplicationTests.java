e1e964d1ea757d8dcec4cb7bb7e540b8586274bf
ahahou3 <wangzhicheng811@gmail.com>
2024-04-24 21:22:08 +0800

    后端初始化

---
+++ back_end/src/test/java/com/ahahou3/user_center/UserCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ahahou3.user_center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
